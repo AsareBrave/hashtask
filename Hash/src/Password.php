9dc154faa91853be08eb8735fb6b4e7b5dd96c4f
Joshua Osagie <josh001pro@gmail.com>
2020-08-08 04:36:04 +0100

    new password generator

--- Hash/src/Password.php
+++ Hash/src/Password.php
@@ -4,144 +4,41 @@ use Hash\Hash;
 
 class Password extends Hash
 {
-    /**
-     * This properties are used dynamically
-     */
-    private $uppercase;
-    private $lowercase;
-    private $numbers;
-    private $special_chars;
-    private $password;
-    private $no_of_str;
-
-    private $white_list = [
-        'numbers',
-        'special_chars',
-    ];
-    public function generate_password($password_length = 6, $options = []): string
+    public function generateStrongPassword($length = 9, $add_dashes = false, $available_sets = 'luds')
     {
-        if (is_array($options)) {
-            foreach ($options as $key => $value) {
-                $this->passwordOptions($key, $value);
+        $sets = array();
+        if (strpos($available_sets, 'l') !== false)
+            $sets[] = 'abcdefghjkmnpqrstuvwxyz';
+        if (strpos($available_sets, 'u') !== false)
+            $sets[] = 'ABCDEFGHJKMNPQRSTUVWXYZ';
+        if (strpos($available_sets, 'd') !== false)
+            $sets[] = '23456789';
+        if (strpos($available_sets, 's') !== false)
+            $sets[] = '!@#$%&*?';
 
-
-                $this->no_of_str = $password_length / count($options);
-
-                if (isset($options['numbers'])) {
-                    $this->password = str_shuffle($this->generate_alphabets($this->no_of_str) . $this->generate_numbers($this->no_of_str));
-                }
-
-                if (isset($options['special_chars'])) {
-                    $this->password = str_shuffle($this->generate_alphabets($this->no_of_str) . $this->generate_special_characters($this->no_of_str));
-                }
-
-                if (isset($options['numbers']) && isset($options['special_chars'])) {
-                    $this->password = str_shuffle($this->generate_alphabets($this->no_of_str) . $this->generate_numbers($this->no_of_str) . $this->generate_special_characters($this->no_of_str));
-                }
-
-                if (!isset($options['numbers']) && !isset($options['special_chars'])) {
-                    $this->password = str_shuffle($this->generate_characters($this->no_of_str));
-                }
-            }
-
-            return $this->password;
-        } else {
-            return 'Expected an array!';
-        }
-    }
-
-
-    private function passwordOptions($key, $value): void
-    {
-        if (in_array($key, $this->white_list)) {
-            $property = $key;
-            $this->$property = $value;
-        } else {
-            die('Invalid password option');
-        }
-    }
-
-    /**
-     * returns all string characters
-     * @todo you can add any I missed
-     * @return string
-     */
-    private function generate_characters($length)
-    {
-        $data = ['A' => 'A', 'B' => 'B', 'C' => 'C', 'D' => 'D', 'E' => 'E', 'F' => 'F', 'G' => 'G', 'H' => 'H', 'I' => 'I', 'J' => 'J', 'K' => 'K', 'L' => 'L', 'M' => 'M', 'N' => 'N', 'O' => 'O', 'P' => 'P', 'Q' => 'Q', 'R' => 'R', 'S' => 'S', 'T' => 'T', 'U' => 'U', 'V' => 'V', 'W' => 'W', 'Y' => 'Y', 'Z' => 'Z', 'a' => 'a', 'b' => 'b', 'c' => 'c', 'd' => 'd', 'e' => 'e', 'f' => 'f', 'g' => 'g', 'h' => 'h', 'i' => 'i', 'j' => 'j', 'k' => 'k', 'l' => 'l', 'm' => 'm', 'n' => 'n', 'o' => 'o', 'p' => 'p', 'q' => 'q', 'r' => 'r', 's' => 's', 't' => 't', 'u' => 'u', 'v' => 'v', 'w' => 'w', 'y' => 'y', 'z' => 'z', '!' => '!', '@' => '@', '#' => '#', '$' => '$', '%' => '%', '^' => '^', '*' => '*', '(' => '(', ')' => ')', '+' => '+', '<' => '<', '>' => '>', '?' => '?', '/' => '/', '[' => '[', ']' => ']', '{' => '{', '}' => '}', 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9, 0 => 0];
-
-        if ($length > count($data)) {
-            $length = count($data);
+        $all = '';
+        $password = '';
+        foreach ($sets as $set) {
+            $password .= $set[array_rand(str_split($set))];
+            $all .= $set;
         }
 
-        for ($i = 0; $i <= count($data); $i++) {
-            $str_arr = array_rand($data, $length);
+        $all = str_split($all);
+        for ($i = 0; $i < $length - count($sets); $i++)
+            $password .= $all[array_rand($all)];
+
+        $password = str_shuffle($password);
+
+        if (!$add_dashes)
+            return $password;
+
+        $dash_len = floor(sqrt($length));
+        $dash_str = '';
+        while (strlen($password) > $dash_len) {
+            $dash_str .= substr($password, 0, $dash_len) . '-';
+            $password = substr($password, $dash_len);
         }
-        $generated_string = (array)$str_arr;
-
-        return implode('', $generated_string);
-    }
-
-    /**
-     * returns all alphabets
-     * 
-     * @return string
-     */
-    private function generate_alphabets($length)
-    {
-        $data = ['A' => 'A', 'B' => 'B', 'C' => 'C', 'D' => 'D', 'E' => 'E', 'F' => 'F', 'G' => 'G', 'H' => 'H', 'I' => 'I', 'J' => 'J', 'K' => 'K', 'L' => 'L', 'M' => 'M', 'N' => 'N', 'O' => 'O', 'P' => 'P', 'Q' => 'Q', 'R' => 'R', 'S' => 'S', 'T' => 'T', 'U' => 'U', 'V' => 'V', 'W' => 'W', 'Y' => 'Y', 'Z' => 'Z', 'a' => 'a', 'b' => 'b', 'c' => 'c', 'd' => 'd', 'e' => 'e', 'f' => 'f', 'g' => 'g', 'h' => 'h', 'i' => 'i', 'j' => 'j', 'k' => 'k', 'l' => 'l', 'm' => 'm', 'n' => 'n', 'o' => 'o', 'p' => 'p', 'q' => 'q', 'r' => 'r', 's' => 's', 't' => 't', 'u' => 'u', 'v' => 'v', 'w' => 'w', 'y' => 'y', 'z' => 'z'];
-
-        if ($length > count($data)) {
-            $length = count($data);
-        }
-
-        for ($i = 0; $i <= count($data); $i++) {
-            $str_arr = array_rand($data, $length);
-        }
-        $generated_string = (array)$str_arr;
-
-        return implode('', $generated_string);
-    }
-
-    /**
-     * returns all numbers
-     * 
-     * @return string
-     */
-    private function generate_numbers($length)
-    {
-        $data = [1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9, 0 => 0];
-
-        if ($length > count($data)) {
-            $length = count($data);
-        }
-
-        for ($i = 0; $i <= count($data); $i++) {
-            $str_arr = array_rand($data, $length);
-        }
-        $generated_string = (array)$str_arr;
-
-        return implode('', $generated_string);
-    }
-
-    /**
-     * returns all special characters
-     * @todo you can add any I missed
-     * @return string
-     */
-    private function generate_special_characters($length)
-    {
-        $data = ['!' => '!', '@' => '@', '#' => '#', '$' => '$', '%' => '%', '^' => '^', '*' => '*', '(' => '(', ')' => ')', '+' => '+', '<' => '<', '>' => '>', '?' => '?', '/' => '/', '[' => '[', ']' => ']', '{' => '{', '}' => '}'];
-
-        if ($length > count($data)) {
-            $length = count($data);
-        }
-
-        for ($i = 0; $i <= count($data); $i++) {
-            $str_arr = array_rand($data, $length);
-        }
-        $generated_string = (array)$str_arr;
-
-        return implode('', $generated_string);
+        $dash_str .= $password;
+        return $dash_str;
     }
 }
