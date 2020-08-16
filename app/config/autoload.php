a840e5906f72d16b7b4aa48cda6a3d5eed26215b
Joshua Osagie <josh001pro@gmail.com>
2020-08-16 04:27:05 +0100

    set up dotenv

--- app/config/autoload.php
+++ app/config/autoload.php
@@ -3,6 +3,14 @@ foreach (glob("../../app/Controllers/Hash/*.php") as $filename) {
 	require_once($filename);
 }
 
+foreach (glob("../../app/Controllers/DotEnv/Exceptions/*.php") as $filename) {
+	require_once($filename);
+}
+
+foreach (glob("../../app/Controllers/DotEnv/*.php") as $filename) {
+	require_once($filename);
+}
+
 foreach (glob("../../model/DB/*.php") as $filename) {
 	require_once($filename);
 }
