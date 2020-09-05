b22e95f5fde5aac13162963aa827001982aa625b
Joshua Osagie <josh001pro@gmail.com>
2020-09-05 23:54:00 +0100

    include phpmailer

--- mail.php
+++ mail.php
@@ -1,3 +1,11 @@
 <?php
 
-require_once './app/Controllers/Mail/Mail.php';
+use PHPMailer\PHPMailer\PHPMailer;
+
+require_once './app/Controllers/Mail/PHPMailer.php';
+require_once './app/Controllers/Mail/SMTP.php';
+require_once './app/Controllers/Mail/Exception.php';
+
+$mail = new PHPMailer(true);
+
+var_dump($mail);
